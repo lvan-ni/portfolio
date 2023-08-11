d82b5c6cef2f943d48c0b583ebda288df601dff4
gnilvan <gnilvan@users.noreply.github.com>
2023-08-11 22:05:09 +0200

    favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
       <body className={spaceGrotesk.className}>
         <Header />
         {children}
