14839af82935bc1b6aaebe5d57811470e5081baf
gnilvan <gnilvan@users.noreply.github.com>
2023-08-12 11:38:59 +0200

    style do not apply fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-// import Hero from "@/Components/Hero";
+import Hero from "@/app/Components/Hero";
 import Projects from "@/app/Components/Projects";
 import About from "@/app/Components/About";
 
@@ -15,31 +15,8 @@ const Home = async () => {
   if (!introContent) return null;
   return (
     <main className="bg-core-stone text-core-jet">
-      <section id="hero" className="p-xs lg:px-Dxs lg:py-sm">
-        <h1 className="text-header-lg lg:text-header-lg-D">Lvan Ni</h1>
-        <h1 className="text-header-lg lg:hidden">_Designer</h1>
-        <h1 className="text-header-lg lg:hidden">_Developer</h1>
-        <h1 className="hidden lg:block lg:text-header-lg-D">
-          _Designer_Developer
-        </h1>
-      </section>
-
-      <section className="p-xs lg:px-Dxs lg:py-sm">
-        <h3 className="text-body-lg lg:text-body-lg-D">
-          {documentToReactComponents(introContent)}
-        </h3>
-      </section>
-
-      <section className="p-xs lg:px-Dxs lg:py-sm">
-        <a
-          href="mailto:hello@lvanni.org"
-          className="text-body-md lg:text-body-md-D text-core-jet hover:text-user-blue hover:border-user-blue email"
-        >
-          hello@lvanni.org
-        </a>
-      </section>
-      {/* <Hero />
-      <Projects /> */}
+      <Hero />
+      <Projects />
       <About /> 
     </main>
   );
