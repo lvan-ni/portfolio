8d93b645f40978654bc5ef250d18e38b5eae43c1
lvan-ni <g.nilvan@icloud.com>
2023-12-18 22:49:22 +0100

    Refactor blog page layout

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import BackNav from '@/components/BackNav/BackNav';
 import APIBasics from '@/components/GSAP/APIBasics/APIBasics';
 import AdditionalMethods from '@/components/GSAP/AdditionalMethods/AdditionalMethods';
 
 const blog = () => {
   return (
-    <div className='py-sm px-xs md:px-sm md:py-xs lg:px-Dxs 2xl:px-Dxl'>
-      <header className='flex justify-between items-center'>
-        <BackNav />
-        <h2 className='text-header-md md:text-header-T lg:text-header-md-D'>
+    <>
+      <section className='px-xs py-sm md:px-sm md:py-xs lg:px-Dxs lg:py-md 2xl:px-Dxl'>
+        <h2
+          className="text-header-md md:text-header-T lg:text-header-md-D"
+        >
           _Blog
         </h2>
-      </header>
-      <APIBasics />
-      <AdditionalMethods />
-    </div>
+      </section>
+      <section className='flex flex-col gap-4'>
+        <APIBasics />
+        <AdditionalMethods />
+      </section>
+    </>
   );
 };
 
