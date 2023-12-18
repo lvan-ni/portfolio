35cf3338ad73c0355febfe553531b8155deeff6d
lvan-ni <g.nilvan@icloud.com>
2023-12-18 22:49:26 +0100

    Refactor GSAP animation in AdditionalMethods component

--- src/components/GSAP/AdditionalMethods/AdditionalMethods.tsx
+++ src/components/GSAP/AdditionalMethods/AdditionalMethods.tsx
@@ -1,35 +1,41 @@
 'use client';
-import React, { useEffect } from 'react';
+import React, { useLayoutEffect } from 'react';
 import logo from '@/assets/logo2.svg';
-import grow from '@/assets/logo-grow.svg';
 import Image from 'next/image';
 import gsap from 'gsap';
 
 const AdditionalMethods = () => {
+  const lvanRef = React.useRef() as React.MutableRefObject<HTMLImageElement>;
 
-  useEffect(() => {
-    gsap.to('.logo2', {
-      duration: 1.5,
-      opacity: 0,
-      scale: 0.3,
-      ease: 'back',
+  useLayoutEffect(() => {
+    let lvan = gsap.context(() => {
+      gsap.to(lvanRef.current, {
+        duration: 1.5,
+        opacity: 0,
+        scale: 0.3,
+        ease: 'back',
+      });
     });
+    return () => lvan.revert();
   }, []);
 
   return (
-    <div className='p-4'>
-      <Image
-        src={logo}
-        alt='logo'
-        className='logo2'
-        priority={true}
-      />
-      {/* <Image
+    <div className='mx-sm md:mx-md lg:mx-Dxs 2xl:mx-Dsm'>
+      <h1>Addtional Methods</h1>
+      <div className='p-4'>
+        <Image
+          src={logo}
+          alt='logo'
+          priority={true}
+          ref={lvanRef}
+        />
+        {/* <Image
         src={grow}
         alt='grow'
         className='grow'
         priority={true}
       /> */}
+      </div>
     </div>
   );
 };
