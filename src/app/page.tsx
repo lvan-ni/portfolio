5abdba893e8963117fb1ce8cd50b743bddee595e
lvan-ni <g.nilvan@icloud.com>
2023-12-18 22:49:10 +0100

    Remove unused components from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
 import Hero from '@/components/Hero/Hero';
 import Projects from '@/components/Projects/Projects';
-import About from '@/components/About/About';
-import Footer from '@/components/Footer/Footer';
-import Header from '@/components/Header/Header';
 
 const Home = async () => {
   return (
     <>
-      <Header />
       <main className='bg-core-stone text-core-jet'>
         <Hero />
         <Projects />
-        <About />
       </main>
-      <Footer />
     </>
   );
 };
