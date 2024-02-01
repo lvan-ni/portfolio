96b7ee71007df817e73c12f99c087afca82e29bb
lvan-ni <g.nilvan@icloud.com>
2024-02-01 18:14:33 +0100

    Add react-markdown library and update Hero component

--- src/app/Components/Hero.tsx
+++ src/app/Components/Hero.tsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { getPortfolioHome } from "@/lib/contentful";
+import React from 'react';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import ReactMarkdown from 'react-markdown';
+import { getPortfolioHome } from '@/lib/contentful';
+import { render } from 'react-dom';
 
 const Hero = async () => {
   const data = await getPortfolioHome(
@@ -12,24 +14,32 @@ const Hero = async () => {
   if (!introContent) return null;
 
   return (
-    <div id="hero">
+    <div id='hero'>
       {/* HERO Mobile 320 */}
-      <div className="md:hidden lg:hidden">
-        <section className="p-xs">
-          <h1 className="text-header s:text-header-lg">Lvan Ni</h1>
-          <h1 className="text-header s:text-header-lg">_Design_Dev</h1>
+      <div className='md:hidden lg:hidden'>
+        <section className='p-xs'>
+          <h1 className='text-header s:text-header-lg'>Lvan Ni</h1>
+          <h1 className='text-header s:text-header-lg'>_Design_Dev</h1>
         </section>
 
-        <section className="p-xs">
-          <h3 className="text-body-lg">
-            {documentToReactComponents(introContent)}
-          </h3>
+        <section className='p-xs'>
+          <p className='text-body-lg'>
+            As a Full-Stack Developer and UX designer, I approach problems from
+            a combined perspective, crafting intuitive and effective solutions.
+          </p>
+          <br />
+          <p className='text-body-lg'>
+            Currently consulting with a startup redesigning and rebuilding their
+            user experience and web app. In my free time, I&apos;m delving into
+            GSAP, excited to bring a new dimension of dynamic interactive
+            animations to future projects.
+          </p>
         </section>
 
-        <section className="p-xs">
+        <section className='p-xs'>
           <a
-            href="mailto:lvan.ni@appliedtechnology.se"
-            className="text-body-md text-core-jet hover:text-user-blue hover:border-user-blue email"
+            href='mailto:lvan.ni@appliedtechnology.se'
+            className='text-body-md text-core-jet hover:text-user-blue hover:border-user-blue email'
           >
             Explore Your Vision
           </a>
@@ -37,22 +47,30 @@ const Hero = async () => {
       </div>
 
       {/* HERO Tablet 768 */}
-      <div className="hidden md:block lg:hidden">
-        <section className="md:px-sm md:py-xs">
-          <h1 className="text-header-D">Lvan Ni</h1>
-          <h1 className="text-header-D">_Designer_Developer</h1>
+      <div className='hidden md:block lg:hidden'>
+        <section className='md:px-sm md:py-xs'>
+          <h1 className='text-header-D'>Lvan Ni</h1>
+          <h1 className='text-header-D'>_Designer_Developer</h1>
         </section>
 
-        <section className="px-sm py-xs">
-          <h3 className="text-body-md-D">
-            {documentToReactComponents(introContent)}
-          </h3>
+        <section className='px-sm py-xs'>
+          <p className='text-body-md-D'>
+            As a Full-Stack Developer and UX designer, I approach problems from
+            a combined perspective, crafting intuitive and effective solutions.
+          </p>
+          <br />
+          <p className='text-body-md-D'>
+            Currently consulting with a startup redesigning and rebuilding their
+            user experience and web app. In my free time, I&apos;m delving into
+            GSAP, excited to bring a new dimension of dynamic interactive
+            animations to future projects.
+          </p>
         </section>
 
-        <section className="md:px-sm md:py-xs">
+        <section className='md:px-sm md:py-xs'>
           <a
-            href="mailto:hello@lvanni.org"
-            className="md:text-body text-core-jet hover:text-user-blue hover:border-user-blue email"
+            href='mailto:hello@lvanni.org'
+            className='md:text-body text-core-jet hover:text-user-blue hover:border-user-blue email'
           >
             hello@lvanni.org
           </a>
@@ -60,22 +78,30 @@ const Hero = async () => {
       </div>
 
       {/* HERO DESKTOP 1024+ */}
-      <div className="hidden lg:block">
-        <section className="lg:px-Dxs lg:py-sm 2xl:px-Dxl">
-          <h1 className="lg:text-header-lg-D">Lvan Ni</h1>
-          <h1 className="lg:text-header-lg-D">_Designer_Developer</h1>
+      <div className='hidden lg:block'>
+        <section className='lg:px-Dxs lg:py-sm 2xl:px-Dxl'>
+          <h1 className='lg:text-header-lg-D'>Lvan Ni</h1>
+          <h1 className='lg:text-header-lg-D'>_Designer_Developer</h1>
         </section>
 
-        <section className="lg:px-Dxs lg:py-sm 2xl:px-Dxl">
-          <h3 className="lg:text-body-lg-D">
-            {documentToReactComponents(introContent)}
-          </h3>
+        <section className='lg:px-Dxs lg:py-sm 2xl:px-Dxl'>
+          <p className='text-body-md-D'>
+            As a Full-Stack Developer and UX designer, I approach problems from
+            a combined perspective, crafting intuitive and effective solutions.
+          </p>
+          <br />
+          <p className='lg:text-body-lg-D'>
+            Currently consulting with a startup redesigning and rebuilding their
+            user experience and web app. In my free time, I&apos;m delving into
+            GSAP, excited to bring a new dimension of dynamic interactive
+            animations to future projects.
+          </p>
         </section>
 
-        <section className="lg:px-Dxs lg:py-sm 2xl:px-Dxl">
+        <section className='lg:px-Dxs lg:py-sm 2xl:px-Dxl'>
           <a
-            href="mailto:hello@lvanni.org"
-            className="lg:text-body-md-D text-core-jet hover:text-user-blue hover:border-user-blue email"
+            href='mailto:hello@lvanni.org'
+            className='lg:text-body-md-D text-core-jet hover:text-user-blue hover:border-user-blue email'
           >
             hello@lvanni.org
           </a>
